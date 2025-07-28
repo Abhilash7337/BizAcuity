0439dea4c62d548f611b8a29e46136da1bc90af5
Abhilash73370 <abhilashpodishetty0705@gmail.com>
2025-07-28 12:54:07 +0530

    server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -48,7 +48,7 @@ try {
 createUploadsDir();
 
 // CORS configuration
-app.use(cors(corsOptions));
+app.use(cors({ origin: '*' }));
 
 // Serve static files from uploads directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
